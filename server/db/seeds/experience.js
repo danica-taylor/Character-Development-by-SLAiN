781c72d0bfc7a8fa9d56a8de1970cf20563bb786
Alysha Dosdos <alyshadosdos@gmail.com>
2024-12-17 18:33:04 +1300

    updated seed date for id 1

--- server/db/seeds/experience.js
+++ server/db/seeds/experience.js
@@ -9,15 +9,9 @@ export async function seed(knex) {
     {
       id: 1,
       name: 'Join Dev Academy Aotearoa',
-      date: '22/06/2024',
+      date: '22/07/2024',
     },
-    { id: 2, 
-      name: 'Applied for an internship', 
-      date: '04/12/2024' 
-    },
-    { id: 3, 
-      name: 'Got into a relationship', 
-      date: '07/01/2023' 
-    },
+    { id: 2, name: 'Applied for an internship', date: '04/12/2024' },
+    { id: 3, name: 'Got into a relationship', date: '07/01/2023' },
   ])
 }
